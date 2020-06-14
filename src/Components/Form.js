0d9b70ca4c0c3f7adfa9035e7c57a4eb27cdcc2a
shuvo151dey <48350056+shuvo151dey@users.noreply.github.com>
2020-06-14 17:48:41 +0530

    Update Form.js

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Form = () => {
     return(
         <React.Fragment>
-            <form method="post" action="/api/update_sheet">
+            <form method="post" action="/api/updateSheet">
                 <label htmlFor="name">Name</label>
                 <input type="text" name="name" />
                 <label htmlFor="phone" name="phone">Phone No</label> 
