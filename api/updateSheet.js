545c5aa220923d114d420672c5da39432e5f3242
shuvo151dey <48350056+shuvo151dey@users.noreply.github.com>
2020-06-14 17:50:20 +0530

    Add files via upload

---
+++ api/updateSheet.js
@@ -0,0 +1,38 @@
+
+//'1DTMN85mKwVv41QQuluNWFh8-Qa_d5OejcHV3IDhzDQ4'
+//1DTMN85mKwVv41QQuluNWFh8-Qa_d5OejcHV3IDhzDQ4
+import { GoogleSpreadsheet } from 'google-spreadsheet';
+import creds from './keys.json';
+
+export default async (req, res) => {
+    const { submit, ...rest} = req.body;
+   try {
+      var { name, phone, address, choice } = JSON.parse(rest);
+   } catch (error) {
+      console.error('Bad API call at sheetAction:', error);
+   }
+   const doc = new GoogleSpreadsheet('1DTMN85mKwVv41QQuluNWFh8-Qa_d5OejcHV3IDhzDQ4');
+   await doc.useServiceAccountAuth(creds);
+   await doc.loadInfo();
+   // console.log(doc.title);
+   const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id]
+   // console.log(sheet.title);
+   // console.log(email);
+   var indiaTime = new Date().toLocaleString("en-US", { timeZone: "Asia/Kolkata" });
+   // indiatime = indiatime.setHours(indiatime.get time.getTimezoneOffset
+   // console.log(indiaTime);
+   try {
+      await sheet.addRow({
+         Timestamp: (new Date()).toString(),
+         IST: indiaTime,
+         Name: name,
+         Address: address,
+         "Phone number": phone,
+         Choice: choice
+      });
+   } catch (error) {
+      console.error(error);
+   }
+   res.end();
+};
+
