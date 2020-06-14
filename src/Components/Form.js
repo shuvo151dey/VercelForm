8905cef9c737b1ec9e2c084f51396f0eb81f797e
shuvo151dey <48350056+shuvo151dey@users.noreply.github.com>
2020-06-14 19:07:54 +0530

    Add files via upload

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const Form = () => {
+    const nameInputRef = useRef();
+    const phoneInputRef = useRef();
+    const addressInputRef = useRef();
+    const submitHandler = (event) => {
+        event.preventDefault;
+        const nameInput = nameInputRef.current.value;
+        const phoneInput = phoneInputRef.current.value;
+        const addressInput = addressInputRef.current.value;
+        fetch('/api/updateSheet',{
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },
+            body: JSON.stringify({
+                name: nameInput,
+                phone: phoneInput,
+                address: addressInput
+            })
+        });
+        nameInputRef.current.value = '';
+        phoneInputRef.current.value = '';
+        addressInputRef.current.value = '';
+    };
     return(
         <React.Fragment>
-            <form method="POST" action="/api/updateSheet">
+            <form method="post" onSubmit={submitHandler}>
                 <label htmlFor="name">Name</label>
-                <input type="text" name="name" />
+                <input type="text" name="name" ref={nameInputRef}/>
                 <label htmlFor="phone" name="phone">Phone No</label> 
-                <input type="tel" name="phone" />
+                <input type="tel" name="phone" ref={phoneInputRef}/>
                 <label htmlFor="address" name="address">Aadhar</label>
-                <input type="text" name="address" />
+                <input type="text" name="address" ref={addressInputRef}/>
                 <button type="submit" name="submit">Submit</button>
             </form>
         </React.Fragment>
